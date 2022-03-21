7aadca15e37bac619e677c22ec3dacbca2f6b500
Picci <enrico.piccinin@gmail.com>
2022-03-21 08:23:43 +0100

    add cwd

--- src/lib/create-node-ts-app.ts
+++ src/lib/create-node-ts-app.ts
@@ -6,6 +6,7 @@ function createNodeTsApp() {
     console.log('Current folder', __dirname);
     const executePath = realpathSync(process.argv[1]);
     console.log('executePath', executePath);
+    console.log('cwd', process.cwd());
 }
 
 createNodeTsApp();
