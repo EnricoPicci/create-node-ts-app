4ba8be50a4b0d12954174cb7e3ac37defad1d47d
Picci <enrico.piccinin@gmail.com>
2022-03-21 08:23:52 +0100

    1.0.7

--- dist-package/lib/create-node-ts-app.js
+++ dist-package/lib/create-node-ts-app.js
@@ -6,5 +6,6 @@ function createNodeTsApp() {
     console.log('Current folder', __dirname);
     const executePath = (0, fs_1.realpathSync)(process.argv[1]);
     console.log('executePath', executePath);
+    console.log('cwd', process.cwd());
 }
 createNodeTsApp();
